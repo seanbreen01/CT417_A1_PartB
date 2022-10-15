93b3e0e72aba8df199f70541550caae0c0378823
Sean Breen <s.breen14@nuigalway.ie>
2022-10-15 14:39:40 +0100

    Main method all but finalised, small revisions may be needed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,13 @@ import org.joda.time.DateTime;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Creating Student objects, setting their courses and dates of birth");
+
         Student stud1 = new Student("Sean", 21);
         Student stud2 = new Student("Mike", 20);
         Student stud3 = new Student("Jack", 22);
         Student stud4 = new Student("Emma", 24);
-        Student stud5 = new Student("Steve", 19);
+        Student stud5 = new Student("Nora", 19);
 
         stud1.setCourse("Electronic & Computer Engineering");
         stud1.setDOB("18/02/2001");
@@ -26,7 +28,8 @@ public class Main {
         stud5.setCourse("Computer Science");
         stud5.setDOB("30/06/2003");
 
-        System.out.println(stud1.getUsername());
+
+        System.out.println("Creating Lecturer objects, and setting their dates of birth");
 
         Lecturer lec1 = new Lecturer("John", 40);                                            //Creating lecturer objects
         Lecturer lec2 = new Lecturer("Ferghal", 55);
@@ -42,6 +45,9 @@ public class Main {
         lec5.setDOB("01/03/1984");
 
 
+        System.out.println("Creating Modules, giving them names, module IDs and the lecturer associated with the module");
+
+
         Module soft = new Module("Programming 3", "CT417", lec1);                           //Creating module objects
         Module soc1 = new Module("System on Chip Design I", "EE451", lec2);
         Module soc2 = new Module("System on Chip Design II","EE452", lec2);
@@ -49,7 +55,7 @@ public class Main {
         Module esap = new Module("Embedded Systems Applications Programming", "EE347", lec4);
         Module dsp  = new Module("Digital Signal Processing", "EE445", lec5);
 
-        System.out.println("Module objects instantiated");
+
 
         lec1.setModuleTeaching(soft);
         lec2.setModuleTeaching(soc1);
@@ -58,8 +64,7 @@ public class Main {
         lec4.setModuleTeaching(esap);
         lec5.setModuleTeaching(dsp);
 
-
-
+        System.out.println("Adding students to different modules");
 
         soft.addStudent(stud1);
         soft.addStudent(stud2);
@@ -67,8 +72,6 @@ public class Main {
         soft.addStudent(stud4);
         soft.addStudent(stud5);
 
-        System.out.println("Adding student that is already enrolled to module to see 'Student already enrolled' message");
-        soft.addStudent(stud1);    //should return that student is already enrolled
 
         soc1.addStudent(stud1);         //Adding students to System on chips modules
         soc1.addStudent(stud3);
@@ -93,37 +96,50 @@ public class Main {
         dsp.addStudent(stud4);
         dsp.addStudent(stud5);
 
+        System.out.println("Adding student that is already enrolled to module to see 'Student already enrolled' message");
+        dsp.addStudent(stud1);    //should return that student is already enrolled
 
 
-
-      // soft.listStudentsEnrolled();
-      // soc2.listStudentsEnrolled();
-      // soc2.listStudentsEnrolled();
+        System.out.println("Creating 2 courseProgrammes, one for electronic and computer engineering, and the other for computer science \n hence populating these with modules as needed");
 
         CourseProgramme ece = new CourseProgramme("Electronic and Computer Engineering", new DateTime(2019,9,5,9,0), new DateTime(2024, 5, 6, 17,0) );
         CourseProgramme compsci = new CourseProgramme("Computer Science", new DateTime(2019,9,5,9,0), new DateTime(2024, 5, 6, 17,0));
 
 
-       ece.addModule(soft);        //Adding created modules, and students to ECE CourseProgramme
-       ece.addModule(soc1);
-       ece.addModule(soc2);
-       ece.addModule(mob);
-       ece.addModule(esap);
-       ece.addModule(dsp);
+        ece.addModule(soft);        //Adding created modules, and students to ECE CourseProgramme
+        ece.addModule(soc1);
+        ece.addModule(soc2);
+        ece.addModule(mob);
+        ece.addModule(esap);
+        ece.addModule(dsp);
 
-       ece.addStudent(stud1);
-       ece.addStudent(stud3);
+        ece.addStudent(stud1);
+        ece.addStudent(stud3);
 
-       compsci.addModule(soft);
-       compsci.addModule(mob);
-       compsci.addModule(dsp);
+        compsci.addModule(soft);
+        compsci.addModule(mob);
+        compsci.addModule(dsp);
 
-       compsci.addStudent(stud2);
-       compsci.addStudent(stud5);
+        compsci.addStudent(stud2);
+        compsci.addStudent(stud5);
 
 
 
         //Start Printing stuff from here on as requested by assignment brief
+
+        System.out.println(ece);    //print basic info about ece CourseProgramme, via overridden toString
+        System.out.println("Modules associated with ECE Course Programme:");
+        ece.printModules();
+        System.out.println("Students enrolled in ECE Course Programme:");
+        ece.printStudentsEnrolled();
+
+        System.out.println(compsci);    //print basic info about ece CourseProgramme, via overridden toString
+        System.out.println("Modules associated with Computer Science Course Programme:");
+        compsci.printModules();
+        System.out.println("Students enrolled in Computer Science Course Programme:");
+        compsci.printStudentsEnrolled();
+
+
 
         soft.listStudentsEnrolled();
         soc1.listStudentsEnrolled();
@@ -131,9 +147,6 @@ public class Main {
         mob.listStudentsEnrolled();
         esap.listStudentsEnrolled();
         dsp.listStudentsEnrolled();
-
-
-
 
 
     }
